bb30fc6903c9a2507f395e2072d4b4999b6da21f
rhage <asya.makarska@gmail.com>
2025-09-09 23:29:37 +0300

    minor fix

--- source/lirs_cache.hpp
+++ source/lirs_cache.hpp
@@ -23,38 +23,38 @@ class cache_t {
 public:
 //------------------- PAGE ----------------------
     struct page_t {
-        int     id;
-        int     content;
+        int     id_;
+        int     content_;
 
-        bool    is_lir;
-        bool    is_in_cache;
-        bool    is_in_stack;
-        bool    is_in_queue;
+        bool    is_lir_;
+        bool    is_in_cache_;
+        bool    is_in_stack_;
+        bool    is_in_queue_;
 
-        std::list<page_t*>::iterator stack_it;  //iterators for stack and qeue
-        std::list<page_t*>::iterator queue_it;
+        std::list<page_t*>::iterator stack_it_;  //iterators for stack_ and qeue
+        std::list<page_t*>::iterator queue_it_;
 
-        page_t(int id_, int content_)
-        : id(id_)
-        , content(content_)
-        , is_lir(false)
-        , is_in_cache(false)
-        , is_in_stack(false)
-        , is_in_queue(false)
+        page_t(int id, int content)
+            : id_{id},
+            content_{content},
+            is_lir_{false},
+            is_in_cache_{false},
+            is_in_stack_{false},
+            is_in_queue_{false}
         {}
     };
 //------------------------------------------------
 
     cache_t(int size_)
-    : max_lirs_cap_(size_)
-    , max_hir_cap_(std::max(1, max_lirs_cap_ / 2))
-    , max_lir_cap_(max_lirs_cap_ - max_hir_cap_)
-    , lir_count_(0)
-    , hits_(0)
+        : max_lirs_cap_{size_},
+        max_hir_cap_{std::max(1, max_lirs_cap_ / 2)},
+        max_lir_cap_{max_lirs_cap_ - max_hir_cap_},
+        lir_count_{0},
+        hits_{0}
     {}
 
     ~cache_t() {
-        for (auto it = page_map.begin(); it != page_map.end(); it++) {
+        for (auto it = page_map_.begin(); it != page_map_.end(); it++) {
             delete it->second;
         }
     }
@@ -68,20 +68,25 @@ public:
     void dump_to_file();
 
 private:
-    int const max_lirs_cap_;
-    int const max_hir_cap_;
-    int const max_lir_cap_;
-    int       lir_count_;
-    int       hits_;
+    const int max_lirs_cap_;
+    const int max_hir_cap_;
+    const int max_lir_cap_;
 
-    std::list <page_t*>              stack;
-    std::list <page_t*>              queue;
-    std::unordered_map<int, page_t*> page_map;  //hash map (for both list and queue)
+    int lir_count_;
+    int hits_;
+
+    std::list <page_t*>              stack_;
+    std::list <page_t*>              queue_;
+    std::unordered_map<int, page_t*> page_map_;
 
 
     status_t miss_case(int page_id, page_t* page);
-    status_t lir_hit(int page_id, page_t* page);
-    status_t hir_hit(int page_id, page_t* page);
+    status_t lir_hit(page_t* page);
+    status_t hir_hit(page_t* page);
+    void     access_dump(page_t* page, int page_id);
+    void     miss_dump(page_t* page, int page_id);
+    void     hit_dump();
+
 
     page_t*  get_page(int page_id);
 
@@ -94,4 +99,4 @@ private:
 };
 } // namespace lirs
 
-#endif //LIRS_CACHE_HPP_
+#endif // LIRS_CACHE_HPP_
